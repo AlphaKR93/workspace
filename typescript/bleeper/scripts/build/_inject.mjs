40c8692a07f8c072daa44ac53d161dab9deff83b
AlphaKR93 <dev@alpha93.kr>
2026-01-30 11:37:00 +0900

    chore: Removed non-library projects

--- typescript/bleeper/scripts/build/_inject.mjs
+++
@@ -1,4 +0,0 @@
-export const ReactFragment = /* #__PURE__ */ Symbol.for("react.fragment");
-
-let __c
-export const BleeperCreateElement = (...args) => (__c)(...args);
--- typescript/bleeper/src/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare global {
-  export const BeeperTexts: ImportMeta;
-}
-
-export {};
